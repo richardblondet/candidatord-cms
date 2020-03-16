9293ddbd3b2c86d0876d08a9c62d98045688f63b
richardblondet <richardblondet@gmail.com>
2020-03-16 11:48:05 -0400

    Adding plugin mapping and advancing in the development

--- plugins/import-bridge/admin/src/components/index.js
+++ plugins/import-bridge/admin/src/components/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -11,7 +12,53 @@ const Row = styled.div`
   padding-top: 18px;
 `;
 
+const Block = ({ children, description, style, title }) => {
+  const Wrapper = styled.div`
+    margin-bottom: 35px;
+    background: #ffffff;
+    padding: 22px 28px 18px;
+    border-radius: 2px;
+    box-shadow: 0 2px 4px #e3e9f3;
+    -webkit-font-smoothing: antialiased;
+  `;
+
+  const Sub = styled.div`
+    padding-top: 0px;
+    line-height: 18px;
+    > p:first-child {
+      margin-bottom: 1px;
+      font-weight: 700;
+      color: #333740;
+      font-size: 1.8rem;
+    }
+    > p {
+      color: #787e8f;
+      font-size: 13px;
+    }
+  `;
+  return (
+    <div className="d-block">
+      <Wrapper style={style}>
+        <Sub>
+          {!!title && (
+            <p>
+              {title}
+            </p>
+          )}
+          {!!description && (
+            <p>
+              {description}
+            </p>
+          )}
+        </Sub>
+        {children}
+      </Wrapper>
+    </div>
+  );
+}
+
 export {
   Container,
-  Row
+  Row,
+  Block
 };
--- plugins/import-bridge/admin/src/containers/HomePage/index.js
+++ plugins/import-bridge/admin/src/containers/HomePage/index.js
@@ -4,31 +4,108 @@
  *
  */
 
-import React, { memo } from 'react';
+import React, { memo, useState, Fragment } from 'react';
 // import PropTypes from 'prop-types';
-import {HeaderNav, LoadingIndicator, PluginHeader} from 'strapi-helper-plugin'
+import {HeaderNav, LoadingIndicator, PluginHeader} from 'strapi-helper-plugin';
+import {Button, InputNumber, InputText, Label, Select} from '@buffetjs/core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import fetch from 'isomorphic-unfetch';
 import pluginId from '../../pluginId';
-import { Container, Row } from '../../components';
+import { Container, Row, Block } from '../../components';
+// const getUrl = to => to ? `/plugins/${pluginId}/${to}` : `/plugins/${pluginId}`;
+const pluginUrl = (baseUrl) => (to) => `plugins/${baseUrl}/${to}`;
+const goTo = pluginUrl(pluginId);
 
-const getUrl = to => to ? `/plugins/${pluginId}/${to}` : `/plugins/${pluginId}`;
+// console.log(goTo('home'));
+
+const SimpleRestClient = () => {
+  const [form, setForm] = useState({ 
+    url: 'https://script.google.com/macros/s/AKfycbyyWi5XF3-1P3AeH1SydzbT4AYrHXyVGb5KDO3ys5koef2CngLc/exec?action=read&table=Sheet1',
+    method: 'GET'
+  });
+
+  const options = ['GET'];
+
+  const updateForm = ( form ) => {
+    setForm(form);
+  }
+
+  const onFieldChangeHandler = ({ name, value }) => {
+    console.log("Change", { name, value });
+    let update = {
+      ...form,
+      [name]: value
+    };
+    updateForm(update);
+  };
+
+  const onSendButtonHandler = ({ preventDefault }) => {
+    console.log("Doing something")
+    preventDefault();
+    if( form.url ){
+      fetch( form.url ).then( res => res.json() ).then( result => {
+        if( result ) {
+          console.log( result );
+        }
+    });
+    }
+  };
+
+  return (
+    <Row>
+      <form>
+        <div className={"row"}>
+          <div className={"col-sm-4"}>
+            <InputText
+              key={"url"}
+              name={"url"}
+              onChange={({ target }) => onFieldChangeHandler( target )}
+              placeholder={"Source URL"}
+              type={"text"}
+              value={form.url}
+            />
+          </div>
+          <div className={"col-sm-2"}>
+            <Select
+              name={"method"}
+              onChange={({ target }) => onFieldChangeHandler( target )}
+              options={options}
+              value={form.method}
+            />
+          </div>
+        </div>
+        <div className={"row mt-3 text-right"}>
+          <div className={"col-sm-6 align-self-center"}>
+            <Button 
+              onClick={onSendButtonHandler}
+              color={"primary"} 
+              icon={<FontAwesomeIcon icon={faPaperPlane} />}
+              >Send</Button>
+          </div>
+        </div>
+      </form>
+    </Row>
+  );
+}
 
 const HomePage = () => {
+  const [val, setValue] = useState('');
+  
   return (
     <Container className={"container-fluid"}>
         <PluginHeader
           title={"Import Bridge"}
-          description={"Import and sync content from a Google Spreadsheet"}
+          description={"Import and sync content from a Restful resource"}
         />
         <Row>
-          <HeaderNav
-            links={[
-              {
-                name: "Sync Data",
-                to: getUrl(''),
-              }
-            ]}
-            style={{marginTop: '4.4rem'}}
-          />
+          <Block
+              title="Sync Data"
+              description="Configure the Import Source & Destination"
+              style={{marginBottom: 12}}
+            >
+            <SimpleRestClient />
+          </Block>
         </Row>
       </Container>
   );
